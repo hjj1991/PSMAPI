1e3f59a55e9d9793a121c6ecb86a13c2beadc12f
hjj1991 <hjj19911@naver.com>
2020-02-27 01:02:51 +0900

    실서버 배포

--- src/main/java/com/psm/api/ServletInitializer.java
+++ src/main/java/com/psm/api/ServletInitializer.java
@@ -1,13 +1,20 @@
 package com.psm.api;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.filter.HiddenHttpMethodFilter;
-
+@EnableJpaAuditing
+@EntityScan(basePackageClasses = {Jsr310JpaConverters.class}, basePackages = {"com.psm.api"})
+@SpringBootApplication(exclude= {MultipartAutoConfiguration.class})
 public class ServletInitializer extends SpringBootServletInitializer {
 
 //	@Override
