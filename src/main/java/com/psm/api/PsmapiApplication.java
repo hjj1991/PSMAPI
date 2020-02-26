b39c6f272b761c19c52219687f1b4efb9f6cd55d
hjj19911@naver.com <hjj19911@naver.com>
2020-02-26 18:39:44 +0900

    그래들 빌드 수정

--- src/main/java/com/psm/api/PsmapiApplication.java
+++ src/main/java/com/psm/api/PsmapiApplication.java
@@ -1,37 +1,37 @@
-package com.psm.api;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
-import org.springframework.context.annotation.Bean;
-import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.crypto.factory.PasswordEncoderFactories;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.filter.HiddenHttpMethodFilter;
-@EnableJpaAuditing
-@EntityScan(basePackageClasses = {Jsr310JpaConverters.class}, basePackages = {"com.psm.api"})
-@SpringBootApplication(exclude= {MultipartAutoConfiguration.class})
-public class PsmapiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(PsmapiApplication.class, args);
-	}
-	
-    /**
-     * HiddenHttpMethodFilter  
-     */
-    @Bean
-    public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
-        HiddenHttpMethodFilter filter = new HiddenHttpMethodFilter();
-        return filter;
-    }
-    
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
-    }
-
-
-}
+//package com.psm.api;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.boot.autoconfigure.domain.EntityScan;
+//import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
+//import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+//import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.web.filter.HiddenHttpMethodFilter;
+//@EnableJpaAuditing
+//@EntityScan(basePackageClasses = {Jsr310JpaConverters.class}, basePackages = {"com.psm.api"})
+//@SpringBootApplication(exclude= {MultipartAutoConfiguration.class})
+//public class PsmapiApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(PsmapiApplication.class, args);
+//	}
+//	
+//    /**
+//     * HiddenHttpMethodFilter  
+//     */
+//    @Bean
+//    public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
+//        HiddenHttpMethodFilter filter = new HiddenHttpMethodFilter();
+//        return filter;
+//    }
+//    
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//    }
+//
+//
+//}
--- src/main/java/com/psm/api/ServletInitializer.java
+++ src/main/java/com/psm/api/ServletInitializer.java
@@ -1,13 +1,40 @@
 package com.psm.api;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 public class ServletInitializer extends SpringBootServletInitializer {
 
+//	@Override
+//	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//		return application.sources(PsmapiApplication.class);
+//	}
+	
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(PsmapiApplication.class);
+		return application.sources(ServletInitializer.class);
 	}
-
+	
+	public static void main(String[] args) {
+		SpringApplication.run(ServletInitializer.class, args);
+	}
+	
+  /**
+  * HiddenHttpMethodFilter  
+  */
+ @Bean
+ public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
+     HiddenHttpMethodFilter filter = new HiddenHttpMethodFilter();
+     return filter;
+ }
+ 
+ @Bean
+ public PasswordEncoder passwordEncoder() {
+     return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+ }
 }
